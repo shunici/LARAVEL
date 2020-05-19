2be49bb856231e3e8fcc7e60812fec2389157935
shunici <32427612+shunici@users.noreply.github.com>
2020-05-19 19:49:35 +0700

    Create step_1.php

---
+++ database/step_1.php
@@ -0,0 +1,82 @@
+<?php 
+
+///////////satu ke banyak ////////////
+// langkah 1
+php artisan make:model Category -m
+
+
+public function up()
+{
+    Schema::create('categories', function (Blueprint $table) {
+        $table->increments('id');
+        $table->string('name');
+        $table->string('description')->nullable();
+        $table->timestamps();
+    });
+}
+
+//langkah 2
+php artisan make:model product -m
+
+
+public function up()
+{
+    Schema::create('products', function (Blueprint $table) {
+        $table->increments('id');
+        $table->string('name');
+        $table->string('description')->nullable();
+        $table->string('stock');
+        $table->string('price');
+        $table->integer('category_id');
+        $table->timestamps();
+    });
+}
+
+//langkah 3 relasi data daro produk ke kategori
+php artisan make:migration add_relationships_to_products_table
+
+
+public function up()
+{
+    Schema::table('products', function (Blueprint $table) {
+        $table->integer('category_id')->unsigned()->change();
+        $table->foreign('category_id')->references('id')->on('categories')
+                ->onUpdate('cascade')->onDelete('cascade');
+    });
+}
+
+public function down()
+{
+    Schema::table('products', function(Blueprint $table) {
+        $table->dropForeign('products_category_id_foreign');
+    });
+​
+    Schema::table('products', function(Blueprint $table) {
+        $table->dropIndex('products_category_id_foreign');
+    });
+​
+    Schema::table('products', function(Blueprint $table) {
+        $table->integer('category_id')->change();
+    });
+}
+
+
+//catatan penting jika idnya bigInteger maka relasinya bigInteger juga
+
+di model product.php  (app/product.php)
+protected $guarded = [];
+    
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+  
+//terakhir
+composer require doctrine/dbal
+aktifkan database
+
+
+
+
+
+/>
