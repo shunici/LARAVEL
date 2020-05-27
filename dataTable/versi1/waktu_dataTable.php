5ae1fe224b63401a3336a60932092ce97becf57f
shunici <32427612+shunici@users.noreply.github.com>
2020-05-27 23:25:07 +0700

    waktu datatable

--- dataTable/versi1/waktu_dataTable.php
+++ dataTable/versi1/waktu_dataTable.php
@@ -11,10 +11,16 @@ posisi di controller
                     ->editColumn('created_at', function ($data) {
                         return  Date::parse($data->created_at)->format('l, j F Y');
                     })->make(true);
+------------------------------------------------------------------  
+//dihitung dari data pertama kali dibuat biasanya created_at
+menggunakan methode diffForHumans();
+// output 10 hari yang lau
+$data = record_stok::all();
+            return DataTables::of($data)                    
+                    ->editColumn('created_at', function ($data) {
+                        return  Date::parse($data->created_at)->diffForHumans();
+                    })->make(true);
   
-// output 
-
-
 
 
 
