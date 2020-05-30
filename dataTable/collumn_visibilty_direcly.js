70a4c52fc7d2fc0cef67e8f9f07aeb6115b80b16
shunici <32427612+shunici@users.noreply.github.com>
2020-05-30 16:17:00 +0700

    kolom langsung tak terlihat

---
+++ dataTable/collumn_visibilty_direcly.js
@@ -0,0 +1,15 @@
+kolom secara langsung hilang
+https://datatables.net/reference/option/columns.visible
+
+$('#example').dataTable( {
+  "columnDefs": [
+    { "visible": false, "targets": 0 }
+  ]
+} );
+
+
+
+
+
+explanation of colvis button
+https://datatables.net/reference/button/colvis
