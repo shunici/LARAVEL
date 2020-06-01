e2290e4aa784ce24593ff53e8c21845e287865ee
shunici <32427612+shunici@users.noreply.github.com>
2020-06-01 16:55:45 +0700

    proses post sederhana js

---
+++ jquery_ajax/get_dan_post/sederhana_post_ajax.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $('#buat_pelanggan').click(function(e){
+      e.preventDefault();
+     
+      $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+      });
+      var CSRF_TOKEN = $('input[name="_token"]').attr('value');
+             $.ajax({
+               type: "POST",      
+               dataType: "json",
+               url: "{{route('create_pelanggan-store')}}",
+               data: {
+                   '_token' : CSRF_TOKEN,
+                   nama : $("input[name='nama']").val(),
+                   keterampilan: $("input[name='no_hp']").val(),
+                   status_pelanggan_radio : $("input[name='status_pelanggan_radio']:checked").val(),
+                   },
+                   success: function (data) {                                     
+                    if($.isEmptyObject(data.error)){                                                                     
+                     console.log(data.sukses);
+                    //  window.location.reload();
+                    }else{
+                      printErrorMsg(data.error);
+                    }                   
+                  }
+              });   
+              //tutup  $.ajax
+   //tutup dokumen dan $form  
+
+    })
+})
+</script>
