1b30b5644487fcad3ea079a49ed9ac8dc3d536ca
shunici <32427612+shunici@users.noreply.github.com>
2020-06-09 02:03:34 +0700

    waktu php

--- waktu/carbon_waktu.php
+++ waktu/carbon_waktu.php
@@ -1,2 +1,15 @@
-  $waktu = Date::now();  
-  tampilkan waktu sekarang
+<?php 
+
+$waktu = Date::now();  
+tampilkan waktu sekarang
+  
+  
+  Date::parse($data->created_at)->format('l, j F Y');
+ \Date::parse($waktu)->format('l, j F Y');
+tampinya Senin, 8 Juni 2020 
+  
+  menggunakan waktu bahasa indonesia data parse laravel
+?>  
+
+
+
