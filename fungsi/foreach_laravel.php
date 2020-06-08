1942b0ddb48062d80bab049526c5ddcec92ad13c
shunici <32427612+shunici@users.noreply.github.com>
2020-06-09 00:24:50 +0700

    cara inisiasi forach pada variabel php laravel

---
+++ fungsi/foreach_laravel.php
@@ -0,0 +1,10 @@
+https://stackoverflow.com/questions/3045619/how-to-store-values-from-foreach-loop-into-an-array
+
+$items = array();
+foreach($group_membership as $username) {
+ $items[] = $username;
+}
+
+print_r($items);
+
+simpan array pada foreach dengan variabel
