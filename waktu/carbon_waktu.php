d06e51a8bd73f3d86be4d1910610c81f3f72860e
shunici <32427612+shunici@users.noreply.github.com>
2020-06-19 22:37:06 +0700

    waktu carboon

--- waktu/carbon_waktu.php
+++ waktu/carbon_waktu.php
@@ -1,14 +1,31 @@
 <?php 
+perlu diperhatikan ketika menggunakan waktu harus disetting dahulu waktu local timezonenya
 
 $waktu = Date::now();  
-tampilkan waktu sekarang
-  
+tampilkan waktu sekarang  
   
   Date::parse($data->created_at)->format('l, j F Y');
  \Date::parse($waktu)->format('l, j F Y');
 tampinya Senin, 8 Juni 2020 
   
   menggunakan waktu bahasa indonesia data parse laravel
+  
+  =====================================  
+$waktu = Date::parse($data->created_at)->diffForHumans();
+dd($waktu);
+// hasil 30 detik yang lalu, sampai 1 jam yang lalu
+=====================================================
+$waktu = Date::parse($data->created_at)->diffInSeconds();
+dd($waktu);
+//hasil dari 1 dan seterusnya mewakili dari detik sejak data dibuat
+fungsi lainnya ::
+ diffInSeconds();  
+diffInMinutes(); 
+diffInHours();  
+diffInDays(); 
+  
+  
+  
 ?>  
 
 
