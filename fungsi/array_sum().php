114296aa229e55454212e5fbc56036ff979385cf
shunici <32427612+shunici@users.noreply.github.com>
2020-06-26 00:42:36 +0700

    suma array data variable

---
+++ fungsi/array_sum().php
@@ -0,0 +1,7 @@
+https://www.w3schools.com/php/phptryit.asp?filename=tryphp_func_array_sum
+//berfungsi untuk menambahkan jumlah vallue pada array
+
+<?php
+$a=array(5,15,25);
+echo array_sum($a);
+?>
