5f209a8fc8652d4064695748f13b06c2b2e9a058
shunici <32427612+shunici@users.noreply.github.com>
2020-06-25 21:22:41 +0700

    read data

--- database/read.php
+++ database/read.php
@@ -2,6 +2,8 @@
 //read paling akhir
  $stok_records = DB::table('record_stoks')->where('bahan_id', 2)->latest()->first();
  
+//read data paling baru
+database::orderBy('created_at', 'DESC')->get(); 
  
  //pilih nama tertentu berdasarkan id foreignkey (berarrti ada array yang dimiliki id foreign key)
  $data = DB::table('record_stoks')->select('input')->where('bahan_id', $id)->get();
