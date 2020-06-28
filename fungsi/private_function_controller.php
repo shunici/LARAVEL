db8b09aa6965b974c6e827abfe48dad72e0f5b6a
shunici <32427612+shunici@users.noreply.github.com>
2020-06-28 15:31:43 +0700

    fungsi di controller dengan uang

---
+++ fungsi/private_function_controller.php
@@ -0,0 +1,15 @@
+//posisi di controlller
+
+format rupiah uang duit php di controller
+ private function rupiah($angka)
+    {
+        $hasil_rupiah = "Rp " . number_format($angka,2,',','.');
+        return $hasil_rupiah;
+       
+    }
+   
+ public function index()
+ {
+  echo $this->rupiah(10000);
+  //hasil Rp. 10.000,-
+ }
