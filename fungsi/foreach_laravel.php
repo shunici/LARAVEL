7edc7f8009aab79829e95389824476eeca1c5e45
shunici <32427612+shunici@users.noreply.github.com>
2020-06-28 16:29:28 +0700

    foreach php laravel

--- fungsi/foreach_laravel.php
+++ fungsi/foreach_laravel.php
@@ -1,10 +1,15 @@
 https://stackoverflow.com/questions/3045619/how-to-store-values-from-foreach-loop-into-an-array
-
+simpan array pada foreach dengan variabel
 $items = array();
 foreach($group_membership as $username) {
  $items[] = $username;
 }
-
 print_r($items);
 
-simpan array pada foreach dengan variabel
+//jika kita ingin menampilkan satu saja (terakhir record) didalam username itu  maka seperti ini
+
+$items = '';
+foreach($group_membership as $username) {
+ $items = $username;
+}
+print_r($items);
