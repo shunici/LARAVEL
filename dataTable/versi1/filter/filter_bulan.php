8a5d4ef005492a32ee4b044117f3bdcc9828c27f
shunici <32427612+shunici@users.noreply.github.com>
2020-06-30 21:01:09 +0700

    filter bulan datatable

--- dataTable/versi1/filter/filter_bulan.php
+++ dataTable/versi1/filter/filter_bulan.php
@@ -18,6 +18,32 @@
 
 //dibagian sini blade tabel
 
+     
+ //controller
+     
+   public function index(Request $request)
+    {
+
+        if($request->ajax())
+        {    
+            if(!empty($request->filter)){
+                $tahun = Carbon::parse($request->filter)->format('Y');
+                $bulan = Carbon::parse($request->filter)->format('m');   
+                 $waktu = \Date::parse(Date::now())->format('l, j F Y');
+                 $data = record_gajih::with('karyawan')->whereYear('created_at', '=', $tahun)
+            ->whereMonth('created_at', '=', $bulan)->get();  
+            }else{
+                $waktu = \Date::parse(Date::now())->format('l, j F Y');
+                $data = record_gajih::with('karyawan')->get();  
+            }                                
+            return DataTables::of($data)->make(true);
+        }   //tutup if  
+        return view ('data_karyawan.record_gajih.index', compact('waktu'));
+    } //tutup function index  
+     
+     
+     
+     
 <script> $('.input-group.date').datetimepicker({
         locale: 'id',
         format: 'YYYY-MM'    
