c83b9275c1e87d4115aff1248ac8ee1354c82e1d
shunici <32427612+shunici@users.noreply.github.com>
2020-07-04 22:43:08 +0700

    sederhana post

--- jquery_ajax/get_dan_post/sederhana_post_ajax.js
+++ jquery_ajax/get_dan_post/sederhana_post_ajax.js
@@ -31,5 +31,46 @@ $(document).ready(function(){
    //tutup dokumen dan $form  
 
     })
-})
+})// tutup
+
+dengan this form
+
+   $('form').submit(function(e){
+        var CSRF_TOKEN = $('input[name="_token"]').attr('value');
+        e.preventDefault();                   
+
+        $.ajax({
+            type: "post",
+            url: "{{route('produk-store')}}",
+            data:new FormData(this),
+            contentType: false,
+            cache: false,
+            processData: false,
+            dataType: "json",
+            success: function (data) {                                     
+
+                if($.isEmptyObject(data.error)){                                                                     
+                     swal({
+                        title: data.sukses+" Berhasil Ditambahkan",
+                        text: "Kategori " +data.kategori,
+                        icon: "success",
+                        button: "Oke",
+                     }).then(function(){ 
+                            window.location.replace(data.url);
+                        }
+                    );
+                }else{
+                printErrorMsg(data.error);
+                }
+            }
+        });
+        function printErrorMsg (msg) {
+            $(".print-error-msg").find("ul").html('');
+            $(".print-error-msg").css('display','block');
+            $.each( msg, function( key, value ) {                                 
+                $(".print-error-msg").find("ul").append('<li>'+value+'</li>');
+            });
+        }
+
+    });
 </script>
