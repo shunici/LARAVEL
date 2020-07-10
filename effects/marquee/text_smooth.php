33b02b352e3faa7e9dea349db4594c1e478cf021
shunici <32427612+shunici@users.noreply.github.com>
2020-07-11 00:24:44 +0700

    Update text_smooth.php

--- effects/marquee/text_smooth.php
+++ effects/marquee/text_smooth.php
@@ -33,3 +33,39 @@
           Hover on me to stop
         </div>
       </div>
+
+full style
+<style>
+	.marquee-parent {
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+  background-color: #ACFA58;
+  height: 30px;
+}
+.marquee-child {
+  display: block;
+  width: 35%;
+  /* width of your text div */
+  height: 20px;
+  /* height of your text div */
+
+  position: absolute;
+  animation: marquee 15s linear infinite; /* change 5s value to your desired speed */
+
+}
+.marquee-child:hover {
+  animation-play-state: paused;
+  background-color: red;
+  cursor: pointer;
+}
+
+@keyframes marquee {
+  0% {
+    left: 100%;
+  }
+  100% {
+    left: -150px /* same as your text width */
+  }
+}
+</style>
