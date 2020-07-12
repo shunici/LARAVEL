272a3cf80299d9b282b84651d232ee4637e0a521
shunici <32427612+shunici@users.noreply.github.com>
2020-07-12 14:38:06 +0700

    Update full-calender.php

--- calender/full-calender.php
+++ calender/full-calender.php
@@ -1,3 +1,6 @@
+versi 7 laravel madhatter tidak bisa
+https://stackoverflow.com/questions/61223570/moving-from-laravel-5-8-to-7x-errors-with-maddhatter-laravel-fullcalendar
+
 // composer require laravel-fullcalendar
 <php 
 'providers' => [
