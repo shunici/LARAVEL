34bd134d6e44c95e8cfe9fcaa97725d306ebfc0b
shunici <32427612+shunici@users.noreply.github.com>
2020-08-24 23:18:43 +0700

    controller

---
+++ hak_akses/spatie/controller/user/userController.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Http\Controllers\user;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Auth;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use App\User;
+use DB;
+class userController extends Controller
+{
+    public function index()
+    {
+        $users = User::orderBy('created_at', 'DESC')->paginate(10);
+        return view('data_user.user.index', compact('users'));
+    }
+
+    public function create()
+    {
+        $role = Role::orderBy('name', 'ASC')->get();
+        return view('data_user.user.create', compact('role'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:100',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+            'role' => 'required|string|exists:roles,name'
+        ]);
+
+        $user = User::firstOrCreate([
+            'email' => $request->email
+        ], [
+            'name' => $request->name,
+            'password' => bcrypt($request->password),
+            'status' => true
+        ]);
+
+        $user->assignRole($request->role);
+        return redirect(route('users.index'))->with(['success' => 'User: <strong>' . $user->name . '</strong> Ditambahkan']);
+    }
+
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        return view('data_user.user.edit', compact('user'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:100',
+            'email' => 'required|email|exists:users,email',
+            'password' => 'nullable|min:6',
+        ]);
+
+        $user = User::findOrFail($id);
+        $password = !empty($request->password) ? bcrypt($request->password):$user->password;
+        $user->update([
+            'name' => $request->name,
+            'password' => $password
+        ]);
+        return redirect(route('data_user.user.index'))->with(['success' => 'User: <strong>' . $user->name . '</strong> Diperbaharui']);
+    }
+
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+        return redirect()->back()->with(['success' => 'User: <strong>' . $user->name . '</strong> Dihapus']);
+    }
+
+    public function roles(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $roles = Role::all()->pluck('name');
+        return view('data_user.user.roles', compact('user', 'roles'));
+    }
+
+    public function setRole(Request $request, $id)
+    {
+        $this->validate($request, [
+            'role' => 'required'
+        ]);
+
+        $user = User::findOrFail($id);
+        $user->syncRoles($request->role);
+        return redirect()->back()->with(['success' => 'Role Sudah Di Set']);
+    }
+
+    public function rolePermission(Request $request)
+    {
+        $role = $request->get('role');
+        $permissions = null;
+        $hasPermission = null;
+
+        $roles = Role::all()->pluck('name');
+
+        if (!empty($role)) {
+            $getRole = Role::findByName($role);
+            $hasPermission = DB::table('role_has_permissions')
+                ->select('permissions.name')
+                ->join('permissions', 'role_has_permissions.permission_id', '=', 'permissions.id')
+                ->where('role_id', $getRole->id)->get()->pluck('name')->all();
+            $permissions = Permission::all()->pluck('name');
+        }
+        return view('data_user.user.role_permission', compact('roles', 'permissions', 'hasPermission'));
+    }
+
+    public function addPermission(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|unique:permissions'
+        ]);
+
+        $permission = Permission::firstOrCreate([
+            'name' => $request->name
+        ]);
+        return redirect()->back();
+    }
+
+    public function setRolePermission(Request $request, $role)
+    {
+        $role = Role::findByName($role);
+        $role->syncPermissions($request->permission);
+        return redirect()->back()->with(['success' => 'Perizinan ke Peran sudah ditambahkan']);
+    }
+
+    public function perizinan ()
+    {
+        $permissions = Permission::all()->pluck('name');
+    }
+
+    public function user_aktif(Request $request, $id)
+    {
+        $user = User::find($id);
+        if($user){
+            $user->status = $request->status;
+            $user->save();
+        }
+        return redirect()->back()->with(['success' => 'Status berhasil di update']);
+    }
+}
