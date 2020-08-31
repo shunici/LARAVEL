74e1599ab5ce6211f1738682e00ab25f2679b3c6
shunici <32427612+shunici@users.noreply.github.com>
2020-09-01 00:56:09 +0700

    hitung array

--- fungsi/array_sum().php
+++ fungsi/array_sum().php
@@ -1,6 +1,7 @@
 https://www.w3schools.com/php/phptryit.asp?filename=tryphp_func_array_sum
 //berfungsi untuk menambahkan jumlah vallue pada array
 
+// hitung array menambahkan array jumlahkan array
 <?php
 $a=array(5,15,25);
 echo array_sum($a);
