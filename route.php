8977cd7140d30246408e16eabc9e144051cc9255
shunici <32427612+shunici@users.noreply.github.com>
2021-03-13 20:44:38 +0700

    Update route.php

--- route.php
+++ route.php
@@ -9,3 +9,19 @@ Route::get('/{id}', 'ProductController@edit');
 Route::put('/{id}', 'ProductController@update');
 Route::delete('/{id}', 'ProductController@destroy');
 });
+
+//untuk melihat yang available
+php artisan route:list
+  
+  //semua route ini harus melalui auth
+  Route::group(['middleware'=>'auth'], function(){
+   Route::get('blog', 'BlogController@index');
+   Route::get('kontak', 'BlogController@kontak');
+   Route::get('profil', 'BlogController@profil');
+});
+
+//dengan resource
+Route::resource('user', 'UserController');
+
+//langsung ke views
+Route::view('/index', 'index')->name('index');
