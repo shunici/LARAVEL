0d752f5ec4a7e6e3089ca3d3f16070510626b439
shunici <32427612+shunici@users.noreply.github.com>
2021-03-13 22:57:40 +0700

    Update array_object.php

--- fungsi/array_object.php
+++ fungsi/array_object.php
@@ -4,3 +4,18 @@
         );    
   $page = (object)$page;
   echo $page->title;
+
+
+// dengan array
+$page = array([
+            'nama' => 'Ronaldo',
+            'asal' => 'portugal'     
+        ],
+        [
+            'nama' => 'Messi',
+            'asal' => 'argentina'     
+        ]
+                                  
+         );    
+         $page = (object)$page;
+// yang harus dengan looping foreach
